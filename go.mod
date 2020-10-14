b94efdd95913d1ce9009f8d15ed9c23eae8c4302
Leonardo Cicconi <6304835+ldcicconi@users.noreply.github.com>
2020-10-13 18:22:25 -0700

    Add profit + fees calculation on sale action

--- go.mod
+++ go.mod
@@ -3,8 +3,6 @@ module github.com/ldcicconi/MidPriceBot
 go 1.13
 
 require (
-	github.com/ldcicconi/sfox-api-lib v0.0.0-20200923164827-1d384b034f16
+	github.com/ldcicconi/sfox-api-lib v0.0.0-20200923185623-8a431e1e812f
 	github.com/shopspring/decimal v1.2.0
 )
-
-replace github.com/ldcicconi/sfox-api-lib => ../sfox-api-lib
--- main.go
+++ main.go
@@ -9,16 +9,28 @@ import (
 	"github.com/shopspring/decimal"
 )
 
+var one = decimal.NewFromFloat(1)
 var OneHundred = decimal.NewFromFloat(100)
 var buyThreshold = decimal.NewFromFloat(-0.03)
 var sellThreshold = decimal.NewFromFloat(0)
+var feeRate = decimal.NewFromFloat(0.0035)
 
 func buy(price decimal.Decimal) {
-	fmt.Println("buying at price", price)
+	fmt.Println("Buying at price", price)
 }
 
-func sell(price decimal.Decimal) {
-	fmt.Println("selling at price", price)
+func sell(sellPrice, buyPrice decimal.Decimal) {
+	profitRate := sellPrice.Sub(buyPrice).Div(buyPrice)
+	// I got this formula by reducing ((1-feeRate)sellPrice - (1+feeRate)buyPrice )/ ((1+feeRate)buyPrice) in WolframAlpha
+	// this takes into account having to pay a fee on each trade (the buy and the sell)
+	profitRateWFees := sellPrice.Mul(one.Sub(feeRate)).Div(buyPrice.Mul(one.Add(feeRate))).Sub(one)
+	fmt.Println("Selling at price", sellPrice, "bought at", buyPrice)
+
+	onePlus := decimal.NewFromFloat(1).Add(profitRateWFees)
+	a := decimal.NewFromFloat(0.01).Mul(onePlus)
+	profitA := a.Mul(onePlus).Sub(a)
+	profitB := one.Mul(onePlus).Sub(one)
+	fmt.Printf("PROFIT STATS:\nProfit rate: %v%%\nProfit rate w/ Fees: %v%%\nProfit on 0.01BTC: %v\nProfit on 1BTC: %v\n\n", profitRate.Mul(OneHundred), profitRateWFees.Mul(OneHundred), profitA, profitB)
 }
 
 func percentDifference(v1, v2 decimal.Decimal) (decimal.Decimal, error) {
@@ -36,6 +48,7 @@ func main() {
 	sfox := sfoxapi.NewSFOXAPI("")
 
 	var inPosition bool
+	var buyPrice decimal.Decimal
 
 	for {
 		// Get the weighted mid price and true mid price
@@ -54,10 +67,11 @@ func main() {
 		if percentDifference.LessThan(buyThreshold) && !inPosition {
 			fmt.Printf("MidPrice: %v WeightedMid: %v PercentDifference: %v\n", midPrice, weightedMidPrice, percentDifference)
 			buy(ob.Asks[0].Price)
+			buyPrice = ob.Asks[0].Price
 			inPosition = true
 		} else if percentDifference.GreaterThan(sellThreshold) && inPosition {
 			fmt.Printf("MidPrice: %v WeightedMid: %v PercentDifference: %v\n", midPrice, weightedMidPrice, percentDifference)
-			sell(ob.Bids[0].Price)
+			sell(ob.Bids[0].Price, buyPrice)
 			inPosition = false
 		}
 		time.Sleep(time.Second)
